e70aab44ef3afe01043150f2f206a8e0f67ce480
Matt McKenna <matt@mtmckenna.com>
2016-01-30 20:43:24 -0800

    More CSS updates

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  unassignedEngineers: Ember.computed('model.engineers.@each.team', function() {
-    return this.store.peekAll('engineer').filter(function(engineer) {
+  freeAgents: Ember.computed('model.engineers.@each.team', function() {
+    var engineers = this.store.peekAll('engineer').filter(function(engineer) {
       return !engineer.get('team').get('id');
     });
+
+    return Ember.Object.create({ engineers: engineers });
   }),
 });
--- app/components/team-listing/component.js
+++ app/components/team-listing/component.js
@@ -2,14 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['js-team', 'team-listing'],
+  classNameBindings: [ 'dragClass' ],
+  dragClass: 'team-listing--deactivated',
 
-  drop(event) {
-    var data = event.dataTransfer.getData('text/data');
-    this.attrs.moveEngineer(data, this.get('team'));
+  dragLeave(event) {
+    event.preventDefault();
+    this.set('dragClass', 'team-listing--deactivated');
   },
 
   dragOver(event) {
     event.preventDefault();
+    this.set('dragClass', 'team-listing--activated');
+  },
+
+  drop(event) {
+    var data = event.dataTransfer.getData('text/data');
+    this.attrs.moveEngineer(data, this.get('team'));
+    this.set('dragClass', 'team-listing--deactivated');
   },
 
   actions: {
